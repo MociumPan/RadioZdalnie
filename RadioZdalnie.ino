ce377cfc1e138c1d7d7ee3187f7679363566e336
Tomasz Ciborowski <mociumpanie.wesele@gmail.com>
2022-03-21 22:18:31 +0100

    dodano aktualizację zegara przez NTC

--- RadioZdalnie.ino
+++ RadioZdalnie.ino
@@ -11,8 +11,17 @@
 
 #include <SPI.h>
 #include <Ethernet.h>
+#include <EthernetUdp.h>
 #include <MCP7940.h>  // Include the MCP7940 RTC library
 
+
+unsigned int localPort = 8888;       // local port to listen for UDP packets
+
+const char timeServer[] = "time.nist.gov"; // time.nist.gov NTP server
+
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+
+byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
 
 MCP7940_Class MCP7940; 
 const uint8_t  SPRINTF_BUFFER_SIZE{32};  // Buffer size for sprintf()x
@@ -30,6 +39,10 @@ IPAddress ip(192, 168, 50, 77);
 // with the IP address and port you want to use
 // (port 80 is default for HTTP):
 EthernetServer server(80);
+// A UDP instance to let us send and receive packets over UDP
+EthernetUDP Udp;
+
+uint8_t   daySync; 
 
 void setup() {
 
@@ -80,6 +93,12 @@ void setup() {
             now.month(), now.day(), now.hour(), now.minute(),
             now.second());                         // date/time with leading zeros
           Serial.println(inputBuffer);
+                         // date/time with leading zeros
+          Serial.println(now.unixtime());
+  Udp.begin(localPort);
+  
+  syncTime();
+  
   // start the server
   server.begin();
   Serial.print("server is at ");
@@ -167,4 +186,83 @@ void loop() {
      //clearing string for next read
      readString="";  
   }
+  if(daySync != now.day() && now.hour() == 0)
+    syncTime();
+}
+
+void syncTime()
+{
+  sendNTPpacket(timeServer); // send an NTP packet to a time server
+
+  // wait to see if a reply is available
+  delay(1000);
+  
+  if (Udp.parsePacket()) {
+    // We've received a packet, read the data from it
+    Udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+
+    // the timestamp starts at byte 40 of the received packet and is four bytes,
+    // or two words, long. First, extract the two words:
+
+    uint32_t highWord = word(packetBuffer[40], packetBuffer[41]);
+    uint32_t lowWord = word(packetBuffer[42], packetBuffer[43]);
+    // combine the four bytes (two words) into a long integer
+    // this is NTP time (seconds since Jan 1 1900):
+    uint32_t secsSince1900 = highWord << 16 | lowWord;
+    Serial.print("Seconds since Jan 1 1900 = ");
+    Serial.println(secsSince1900);
+
+    // now convert NTP time into everyday time:
+    Serial.print("Unix time = ");
+    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+    const uint32_t seventyYears = 2208988800UL;
+    // subtract seventy years:
+    uint32_t epoch = secsSince1900 - seventyYears;
+    // print Unix time:
+    Serial.println(epoch);
+    DateTime       nowrtc(epoch);
+    MCP7940.calibrateOrAdjust(nowrtc);
+
+    // print the hour, minute and second:
+    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
+    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
+    Serial.print(':');
+    if (((epoch % 3600) / 60) < 10) {
+      // In the first 10 minutes of each hour, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
+    Serial.print(':');
+    if ((epoch % 60) < 10) {
+      // In the first 10 seconds of each minute, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.println(epoch % 60); // print the second
+    daySync = nowrtc.day();
+    Serial.print("aktualizacja odbyla sie dnia: ");
+    Serial.println(daySync);
+  }
+}
+
+// send an NTP request to the time server at the given address
+void sendNTPpacket(const char * address) {
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(address, 123); // NTP requests are to port 123
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
 }
